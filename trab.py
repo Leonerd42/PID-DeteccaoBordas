20700ba46a2f4cf497a75866d0bd2e8344adaaf6
leonerd42 <leosoliveira10@gmail.com>
2019-09-23 14:31:00 -0300

    Trabalho quase finalizado

--- trab.py
+++ trab.py
@@ -19,16 +19,23 @@ class Segmento:
     y2 = 0
 # Fim da classe Segmento ----------------
 
+class Dot: 
+    x = 0
+    y = 0
+# Fim da classe dot
+
 # função para aplicar a transformada de Hough
 def HoughLines (img, min_length, max_length):
     transformada = []
-    hipotenusa = math.sqrt(len(img)*len(img) + len(img[0])*len(img[0]))
-    for i in range(int(hipotenusa)*2+1): 
+    hipotenusa = int(round(math.sqrt(len(img)*len(img) + len(img[0])*len(img[0]))))
+    for i in range(int(hipotenusa)*2): 
         linha = []
         for j in range(0,180,1): 
             linha.append(0)
         transformada.append(linha)
     ## Matriz transformada gerada!!!
+
+    #print('Valores da transformada: ',len(transformada), len(transformada[0]))
 
     ## Calculando a transformada de Hough
     for x in range(len(img)): 
@@ -37,7 +44,7 @@ def HoughLines (img, min_length, max_length):
                 for tetha in range(0,180,1): 
                     cosseno = math.cos(tetha * math.pi / 180)
                     seno = math.sin(tetha * math.pi / 180)
-                    p = int(round(x * cosseno + y * seno + len(img)))
+                    p = int(round(x * cosseno + y * seno + hipotenusa))
                     transformada[p][tetha] += 1
     ## Fim do calculo da transformada 
 
@@ -47,15 +54,15 @@ def HoughLines (img, min_length, max_length):
         for tetha in range(len(transformada[rho])):
             if transformada[rho][tetha] > min_length: 
                 pico = Pico()
-                pico.rho = int(round(rho - hipotenusa))
+                pico.rho = int(round(rho - hipotenusa -2))
                 pico.tetha = tetha
                 picos.append(pico)
-
-    return picos            
+    return transformada, picos            
 # ---------------------------------- Fim da função de Hough
 
 # Funçao para verificar se na vizinhança do pixel existe um pixel com a cor color 
 def VerificarVizinhaca (img, x, y, color):
+    #print(x,y,len(img),len(img[0]))
     cor = color
     if img[x][y] == cor: 
         return True
@@ -78,199 +85,184 @@ def VerificarVizinhaca (img, x, y, color):
             return False
 # Fim da funcao de busca na vizinhanca ------------------------------
 
-def BuscarSegmento (img, reta, min_length, gaps, line_color):
+def BuscarReta (img, reta):
     ## Calculando o seno e cosseno de tetha 
+    rho = reta.rho
     tetha = reta.tetha
-    cosseno = int(round(math.cos(tetha * math.pi / 180)))
-    seno = int(round(math.sin(tetha * math.pi / 180)))
-    segmentosReta = []
+    cosseno = math.cos(tetha * math.pi / 180)
+    seno = math.sin(tetha * math.pi / 180)
 
-    ## Seno e cosseno encontrados!!! Função y eh y = ( p - x * cos (tetha) )/ sin (tetha)
-    ## Logo seno não pode ser 0 
-    if seno == 0: 
-        ## Tratar como uma reta vertical 
-        x = reta.rho
-        if reta.rho < 0: 
-             x *= (-1)
-             x = int(round(x/2))            
-        isCounting = False 
-        isSegment  = False 
-        countingGap = False 
-        count_seg = 0
-        count_gap = 0
-        y_inicio = 0
-        y_final = 0
+    reta = []
+
+    if abs(seno) <= 0.2: 
         for y in range(0,len(img[0]),1): 
-            if VerificarVizinhaca(img, x, y, line_color): 
-                ## Existe um segmento de linha
-                if isCounting == False:  # já esta contando o segmento 
-                    isCounting = True
-                count_seg +=1            # Sempre sera contando quando encontrar um pixel da cor color
-                ## Resetando a gap se ela for menor que o tamanho da gap: 
-                if countingGap == True: 
-                    countingGap = False
-                    count_gap = 0
-                    y_final = 0  # Zerando o final do segmento 
-                ## Zerado o contador de gaps 
-
-                ## Verificando se ja passou do comprimento minimo 
-                if count_seg >= min_length and isSegment == False: 
-                    # Salvando o inicio do segmento!!
-                    isSegment = True
-                    y_inicio = y
-                # -- Fim do salvamento do segmento 
-            else:
-                if isCounting == True and isSegment == False: # Se estiver contando, mas ainda não é um segmento  
-                    isCounting = False
-                    count_seg = 0
-                else: 
-                    if isCounting == True and isSegment == True:
-                        ## Encontrado um possivel gap 
-                        if countingGap == False:  ## Primeiro pixel contrario ao color 
-                            countingGap = True
-                            y_final = y            # Salvando o ponto que terminou o segmento 
-                        
-                        count_gap += 1
-
-                        ## Verificando se passou do tamanho da gap 
-                        if count_gap >= gaps:   # Acabou o segmento aqui 
-                            count_gap = 0
-                            count_seg = 0
-                            segmento = Segmento() 
-                            segmento.x1 = x
-                            segmento.y1 = y_inicio
-                            segmento.x2 = x
-                            segmento.y2 = y_final
-                            y_final = 0
-                            isSegment = False
-                            isCounting = False
-                            countingGap = False
-                            segmentosReta.append(segmento)
-                            #print('final!')
-    #-- Fim do tratamento de uma reta vertical     
+            x = int(round(rho - y*seno)/cosseno)
+            if x >= len(img): 
+                x = len(img)-1
+            dot = Dot()
+            dot.x = x 
+            dot.y = y
+            reta.append(dot)
     else: 
-        ## Tratar como uma reta normal 
-        #print('Outras retas')
-        x_inicio = 0
-        y_inicio = 0
-        x_final = 0
-        y_final = 0
-        isCounting = False 
-        isSegment  = False 
-        countingGap = False 
-        count_seg = 0
-        count_gap = 0
-        for x in range(len(img)):
-            y = int(round((reta.rho - x * cosseno) / seno ))
+        for x in range(0,len(img), 1): 
+            y = int(round(rho - x*cosseno)/seno)
+            if y >= len(img[0]):
+                y = len(img[0])-1
+            dot = Dot()
+            dot.x = x
+            dot.y = y
+            reta.append(dot)
+    return reta
+## Fim da procura por retas 
 
-            if VerificarVizinhaca(img, x, y, line_color): 
-                ## Existe um segmento de linha
-                if isCounting == False:  # já esta contando o segmento 
-                    isCounting = True
-                count_seg += 1            # Sempre sera contando quando encontrar um pixel da cor color
-                ## Resetando a gap se ela for menor que o tamanho da gap: 
-                if countingGap == True: 
-                    countingGap = False
-                    count_gap = 0
-                    x_final = 0
-                    y_final = 0  # Zerando o final do segmento 
-                ## Zerado o contador de gaps 
+def BuscarSegmento (img, reta, min, gap):
 
-                ## Verificando se ja passou do comprimento minimo 
-                if count_seg >= min_length and isSegment == False: 
-                    # Salvando o inicio do segmento!!
-                    isSegment = True
-                    x_inicio = x
-                    y_inicio = y
-                # -- Fim do salvamento do segmento 
-            else:
-                if isCounting == True and isSegment == False: # Se estiver contando, mas ainda não é um segmento  
-                    isCounting = False
-                    count_seg = 0
-                else: 
-                    if isCounting == True and isSegment == True:
-                        ## Encontrado um possivel gap 
-                        if countingGap == False:  ## Primeiro pixel contrario ao color 
-                            countingGap = True
-                            x_final = x
-                            y_final = y            # Salvando o ponto que terminou o segmento 
-                        
-                        count_gap += 1
+    isCounting = False 
+    isSegment  = False 
+    countingGap = False 
+    count_seg = 0
+    count_gap = 0
+    x_inicio = 0
+    x_final = 0
+    y_inicio = 0
+    y_final = 0
+    segmentos = []
+    for dot in reta: 
+        x = dot.x 
+        y = dot.y
+        # Encontrando um pixel branco
+        if(VerificarVizinhaca(img,x,y,255)): 
+            # Começa a contar se não estava 
+            if isCounting == False: 
+                isCounting = True
+                x_inicio = x
+                y_inicio = y
+            
+            count_seg += 1  # Acrescenta 1 o contador do tamanho
 
-                        ## Verificando se passou do tamanho da gap 
-                        if count_gap >= gaps:   # Acabou o segmento aqui 
-                            count_gap = 0
-                            count_seg = 0
-                            segmento = Segmento() 
-                            segmento.x1 = x_inicio
-                            segmento.y1 = y_inicio
-                            segmento.x2 = x_final
-                            segmento.y2 = y_final
-                            segmentosReta.append(segmento)
-                            x_final = 0
-                            y_final = 0
-                            isSegment = False
-                            isCounting = False
-                            countingGap = False
-                            print('Salvo um segmento não vertical!', x_inicio, y_inicio, x_final, y_final)
-    return segmentosReta            
+            # Reseta a gap se estiver contando gap
+            if countingGap == True: 
+                countingGap = False
+                count_gap = 0
+                x_final = 0
+                y_final = 0
 
-# Fim da função Buscar Segmento -----------------
+            if count_seg >= min and isSegment == False:
+                isSegment = True
+        else: 
+            if isCounting == True and isSegment == False: 
+                isCounting = False
+                count_seg = 0
+                # n precisa zerar o começo 
+            else: 
+                if isCounting == True and isSegment == True: 
+                    # Encontrado um possivel gap 
+                    if countingGap == False: 
+                        countingGap = True
+                        x_final = x
+                        y_final = y
+                        # salva o final TODO
+                    
+                    # Acrescentando 1 no contador do gap 
+                    count_gap += 1
 
-def Rotacionar (angle, x, y): # Função para rotacionar uma coordenada
+                    if count_gap >= gap or reta.index(dot) == len(reta)-1: 
+                        segmento = Segmento()
+                        segmento.x1 = x_inicio
+                        segmento.y1 = y_inicio
+                        segmento.x2 = x_final
+                        segmento.y2 = y_final
+                        count_gap = 0
+                        count_seg = 0
+                        x_inicio = 0
+                        x_final = 0
+                        y_inicio = 0
+                        y_final = 0
+                        isSegment = False
+                        isCounting = False 
+                        countingGap = False
+                        segmentos.append(segmento) 
+    return segmentos
+
+# Fim da busca dos segmentos 
+
+def Rotacionar (angle, x, y, Tx, Ty): # Função para rotacionar uma coordenada
     tetha = (angle * math.pi / 180)
-    transl_x = 0
-    transl_y = 0
-    x_linha = int(round(x * math.cos(tetha) - y * math.sin(tetha) + transl_x))
-    y_linha = int(round(x * math.sin(tetha) + y * math.cos(tetha) + transl_y))
+    x_linha = int(round(x * math.cos(tetha) - y * math.sin(tetha) + Tx))
+    y_linha = int(round(x * math.sin(tetha) + y * math.cos(tetha) + Ty))
     return x_linha, y_linha
 # Fim da função de rotação -----------------------------------------
+
+def PlotarLinha(img, x1, y1, x2, y2, angulo, tx, ty, cor, line):
+    xy1 = Rotacionar(angulo, x1, y1, tx, ty)
+    xy2 = Rotacionar(angulo, x2, y2, tx, ty)
+    cv2.line(img, (xy1), (xy2), cor, line)
 
 # -------------------------------------------------------------------
 #                     COMEÇO DO PROGRAMA PRINCIPAL
 # -------------------------------------------------------------------
 
-img = cv2.imread('square.png') 
+nome = 'cubo.jpg'
+
+## Variaveis Globais
+## ------- Manipulação do desenho
+anguloRotacao = 90
+TranslacaoX = 115
+TranslacaoY = 0
+
+## Thesholds 
+votacaoHough = 20
+lenghtMinSeg = 8
+lengthGap = 5
+
+## Carregando imagens
+img = cv2.imread(nome) 
+retasImg = cv2.imread(nome) 
+finalImage = cv2.imread(nome) 
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 edges = cv2.Canny(gray,100,200)
 output = edges
 
-segmentos = []
-picos =  HoughLines(edges, 0, 200)
+retas = []
+transformada, picos = HoughLines(edges, votacaoHough, 500)
 
-anguloRotacao = 0
-
-# for pico in picos: 
-#     print (pico.rho, pico.tetha)
-
+# Após encontrados os picos na transformada de Hough, hora de
+# Encontrar as retas, o código abaixo retorna um vetor de pontos
+# aonde os pontos são coordenadas x, y
 for pico in picos:
-    auxiliar = BuscarSegmento(edges,pico,20,5,255)
-    for seg in auxiliar: 
-        print(seg.x1, seg.y1, seg.x2, seg.y2)
-    segmentos.append(auxiliar)
+    retas.append(BuscarReta(edges,pico))
 
-print('Desenhando na imagem!')
-for segmento in segmentos: 
-    for seg in segmento: 
-        #print('here')
-        cv2.line(img, (Rotacionar(anguloRotacao, seg.x1, seg.y1)),(Rotacionar(anguloRotacao,seg.x2, seg.y2)),(0,255,0),2)
+# Com as retas encontradas, é hora de encontrar os segmentos
+# O vetor segmentos possui objetos com x, y final e inicial 
+segmentos = []
+for dots in retas:  
+    reta = [dots[0], dots[-1]]
+    PlotarLinha(retasImg, reta[0].x, reta[0].y, reta[1].x, reta[1].y, anguloRotacao, TranslacaoX, TranslacaoY, (0,255,0), 1)
+    segmentos.append(BuscarSegmento(edges, dots, lenghtMinSeg, lengthGap))
 
-# testes = BuscarSegmento(output, picos[2], 10, 5, 255)
-# for teste in testes: 
-#     print ('Segmento de reta: '), 
-#     print (teste.x1, teste.y1, teste.x2, teste.y2)
-#     cv2.line(img, (teste.x1, teste.y1), (teste.x2, teste.y2), (255,0,0), 1)
-
-finalImage = output
+# Desenhando na imagem 
+for segs in segmentos: 
+    for seg in segs:                                        # tetha, Tx, Ty,    RGB,    thick
+        PlotarLinha(finalImage, seg.x1, seg.y1, seg.x2, seg.y2, anguloRotacao, TranslacaoX, TranslacaoY, (255, 0, 0), 1)
 
 # Mostrando a imagem 
 plt.subplot(231),plt.imshow(img,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.title('Imagem Original'), plt.xticks([]), plt.yticks([])
+
 plt.subplot(232),plt.imshow(gray,cmap = 'gray')
-plt.title('Gray Image'), plt.xticks([]), plt.yticks([])
+plt.title('Imagem Cinza'), plt.xticks([]), plt.yticks([])
+
 plt.subplot(233),plt.imshow(edges,cmap = 'gray')
-plt.title('Bordas'), plt.xticks([]), plt.yticks([])
-plt.subplot(234),plt.imshow(finalImage,cmap = 'gray')
+plt.title('Aplicado o Canny'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(234),plt.imshow(transformada,cmap = 'gray')
+plt.title('Transformada'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(235),plt.imshow(retasImg,cmap = 'gray')
+plt.title('Retas'), plt.xticks([]), plt.yticks([])
+
+plt.subplot(236),plt.imshow(finalImage,cmap = 'gray')
 plt.title('Segmentos'), plt.xticks([]), plt.yticks([])
 
 plt.show()
+# Fim do programa
